bb75fddca12f77d6a5b6dedf713f1ba3d9c88c41
Sreymomcloud1 <sreymomc527@gmail.com>
2026-03-03 01:47:52 +0700

    Added post route for Task 14

--- index.js
+++ index.js
@@ -10,3 +10,8 @@ app.get('/', (req, res) => {
 app.listen(PORT, '0.0.0.0', () => {
     console.log(`Server is running on port ${PORT}`);
 });
+
+// Task 14: Post API
+app.post('/data', (req, res) => {
+    res.json({ message: "Post successful!" });
+});
