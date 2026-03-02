fbd45f757814a3a5288ff0386f75f1bc5ea3613b
Sreymomcloud1 <sreymomc527@gmail.com>
2026-03-02 14:08:29 +0700

    Initial commit -Added GET API

---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+// Task 4: Get API
+app.get('/', (req, res) => {
+    res.send('Hello from my EC2 instance! Pipeline is working.');
+});
+
+app.listen(PORT, '0.0.0.0', () => {
+    console.log(`Server is running on port ${PORT}`);
+});
